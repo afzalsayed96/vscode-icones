6a8e11d8248ecaae5442ac6dd65dae8aa5b02f0e
Afzal Sayed <afzal@keepworks.com>
2020-09-23 23:39:42 +0530

    Init

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1 align="center">
+Icônes
+</h1>
+
+<p align="center">Icon Explorer with <b>Instant</b> searching, powered by <a href="https://github.com/antfu/icones" target="_blank">Icones</a> </p>
+
+## Usage
+
+1. Open command palette with `cmd+shift+p`
+2. Enter command `Icones: Find icons`
+
+![](./screenshots/1.png)
+![](./screenshots/2.png)
+![](./screenshots/3.png)
+![](./screenshots/4.png)
+![](./screenshots/5.png)
+
+<p align="center">
+<sub><em>Dark Mode is now Live!</em></sub>
+</p>
+
+![](./screenshots/6.png)
+
+### Features
+
+- **Instant Fuzzy Searching** _- all are done locally, no web queries!_
+- The **Bag** _- select your icons and pack them into a ready-to-use icon font!_
+- Copy the usage scripts
+- SVGs direct download
+- Mobile friendly
+- Collection bookmarks
+- Categories filters
+- Dark mode
+
+## License & Credits
+
+MIT - Anthony Fu 2020
---
+++ src/extension.ts
@@ -0,0 +1,81 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "icones" is now active!');
+
+	let currentPanel: vscode.WebviewPanel | undefined = undefined;
+
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('icones.find', () => {
+
+		const columnToShowIn = vscode.window.activeTextEditor
+		? vscode.window.activeTextEditor.viewColumn
+		: undefined;
+
+		if (currentPanel) {
+			// If we already have a panel, show it in the target column
+			currentPanel.reveal(columnToShowIn);
+		} else {
+			// Otherwise, create a new panel
+
+       currentPanel = vscode.window.createWebviewPanel(
+        'icones', // Identifies the type of the webview. Used internally
+        'Icones', // Title of the panel displayed to the user
+        vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+        {
+					enableScripts: true,
+					retainContextWhenHidden: true
+				}
+      );
+			currentPanel.webview.html = getWebviewContent();
+
+			// Reset when the current panel is closed
+			currentPanel.onDidDispose(
+				() => {
+					currentPanel = undefined;
+				},
+				null,
+				context.subscriptions
+			);
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>React Icons</title>
+		<style>
+		.full {
+			height: 100vh;
+			width: 100vw;
+			margin: 0;
+			border: 0;
+			outline: none;
+		}
+		
+		</style>
+</head>
+<body class="full">
+    <iframe src="https://icones-sandy.vercel.app/" class="full" sandbox="allow-scripts allow-same-origin">/>
+</body>
+</html>`;
+}
